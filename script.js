6c48242ccbb60615b4ef50f4e45f27fea35a3c3c
Mikhail Pestov <mnpestov@gmail.com>
2022-07-10 22:21:56 +0300

    mesto

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ let editBtn = page.querySelector('.button_type_edit');
 let saveBtn = page.querySelector('.button_type_save');
 let closeBtn = page.querySelector('.popup__close');
 let heart = page.querySelectorAll('.element__like');
+let inputName = page.querySelector('.popup__input_type_name');
+let inputAboutMe = page.querySelector('.popup__input_type_about-me');
+let profileName = page.querySelector('.profile__name');
+let aboutMe = page.querySelector('.profile__about-me');
 
 function openPopup() {
   let popup = page.querySelector('.popup');
   popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputAboutMe.value = aboutMe.textContent;
 }
 
 function closePopup() {
@@ -16,21 +22,19 @@ function closePopup() {
 }
 
 function saveAndClosePopup() {
-  let inputName = document.querySelector('.popup__input_type_name');
-  let inputAboutMe = document.querySelector('.popup__input_type_about-me');
-  let name = document.querySelector('.profile__name');
-  let aboutMe = document.querySelector('.profile__about-me');
-  name.textContent = inputName.value;
+  profileName.textContent = inputName.value;
   aboutMe.textContent = inputAboutMe.value;
   closePopup();
+}
+function putLike(index) {
+  heart[index].classList.toggle('element__like_active');
+  heart[index].classList.toggle('opacity-transition_type_05');
 }
 
 editBtn.addEventListener('click', openPopup);
 saveBtn.addEventListener('click', saveAndClosePopup);
 closeBtn.addEventListener('click', closePopup);
+
 for (let i = 0; i < heart.length; i++) {
-  heart[i].addEventListener('click', function(){
-    heart[i].classList.toggle('element__like_active');
-    heart[i].classList.toggle('opacity-transition_type_05');
-  });
+  heart[i].addEventListener('click', () => putLike(i));
 }
