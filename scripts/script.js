8b49fd5ef209c7981c999e8473e416d63281511d
Mikhail Pestov <mnpestov@gmail.com>
2022-07-25 17:15:05 +0300

    hotfix

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,6 @@ const page = document.querySelector('.page');
 const popupCardOpenButton = page.querySelector('.button_type_add');
 const popupCardEditButton = page.querySelector('.button_type_edit');
 const popupCardCloseButton = page.querySelectorAll('.popup__close');
-// const popupCardSaveButton = page.querySelector('.button_type_save');
-// const popupCardCreateButton = page.querySelector('.button_type_create');
 const inputName = page.querySelector('.popup__input_type_name');
 const inputAboutMe = page.querySelector('.popup__input_type_about-me');
 const inputNameOfImage = page.querySelector('.popup__input_type_name-of-image');
@@ -14,32 +12,6 @@ const popupEditForm = page.querySelector('.popup_type_edit');
 const popupAddForm = page.querySelector('.popup_type_add');
 const popupImageForm = page.querySelector('.popup_type_image');
 const elements = page.querySelector('.elements-list');
-// const initialCards = [
-//   {
-//     name: 'Архыз',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//   },
-//   {
-//     name: 'Челябинская область',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//   },
-//   {
-//     name: 'Иваново',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//   },
-//   {
-//     name: 'Холмогорский район',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//   },
-//   {
-//     name: 'Байкал',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//   }
-// ];
 
 function openOrClosePopup(form) {
   form.classList.toggle('popup_opened');
